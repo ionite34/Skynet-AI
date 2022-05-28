095fb6e7ebf2c9b8c24beb873f065c6a56e9ae36
ionite34 <dev@ionite.io>
2022-05-27 23:05:44 -0400

    Token change

--- bot_skynet.py
+++ bot_skynet.py
@@ -6,4 +6,4 @@ def run():
     # Create bot
     skynet_bot = BotClient()
     # Run
-    skynet_bot.run(os.environ['DISCORD_TOKEN_SKYNET_BETA'])
+    skynet_bot.run(os.environ['DISCORD_TOKEN_SKYNET'])
